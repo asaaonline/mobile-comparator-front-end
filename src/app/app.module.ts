6a1fcfef2635e706b1412c1160e0d0d29651dfb4
asela <ecomsearchonline@gmail.com>
2020-02-16 00:11:47 +0530

    add dependencies and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {AppRoutingModule} from './app-routing.module';
 import {HomePageComponent} from './home-page/home-page.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,9 @@ import {HomePageComponent} from './home-page/home-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
